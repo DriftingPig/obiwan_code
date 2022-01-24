57037226482365ba03a01325b89a65e8061666b7
Hui Kong <driftingpig@Huis-MBP.attlocal.net>
2022-01-23 22:18:22 -0500

    obiwan scarlet

---
+++ production_run/py-scarlet/legacypipe/.ipynb_checkpoints/fit_scarlet-checkpoint.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scarlet
+
+def coadd_img():
+    pass
+
+def tractor2scarlet():
+    pass
+
+def scarlet2tractor():
+    pass
+
+def measure():
+    pass
+
+def scarlet_fiting(blobmap,blobsrc,blobslice,tim):
+    import pdb;pdb.set_trace()
+    pass
---
+++ production_run/py-scarlet/legacypipe/fit_scarlet.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scarlet
+
+def coadd_img():
+    pass
+
+def tractor2scarlet():
+    pass
+
+def scarlet2tractor():
+    pass
+
+def measure():
+    pass
+
+def scarlet_fiting(blobmap,blobsrc,blobslice,tim,targetwcs):
+    import pdb;pdb.set_trace()
+    pass
+
+class one_obiwan_scarlet():
+    def __init__(self, tim, blobmap, slicei):
+        
+        
